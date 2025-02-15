9de8a53cb43cc64caf7d2aaf1d40043f3158d082
Mohammad-Hussain <mohammadhussainalizada47@gmail.com>
2025-02-15 22:47:29 +0430

    emploee managment updated

--- connection.php
+++
@@ -1,9 +0,0 @@
-<?php
-	
-	$con = mysqli_connect("localhost", "root", "" , "business_company");
-
-if(!isset($_SESSION)) {
-	session_start();
-}
-	
-?>
--- employee.php
+++
@@ -1,7 +0,0 @@
-<?php require_once("top.php"); ?>
-<?php require_once("restrict.php"); ?>
-
-
-
-
-<?php require_once("footer.php"); ?>
